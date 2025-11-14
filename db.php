63de07be20a38069349a61259f184c1110924013
KJValenzuela <kylejohnvalenzuela@gmail.com>
2025-11-14 16:38:01 +0800

    dbconfig

--- db.php
+++ db.php
@@ -1,11 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "project";
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$user = "6e640300738180007ff81030191e";
+$pass = "06916e64-0300-7507-8000-d6ec482bfbd7";
+$db   = "project1";
+$port = 10272;
 
 // Create connection
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 // Check connection
 if ($conn->connect_error) {
--- login_success.php
+++ login_success.php
@@ -2,7 +2,7 @@
 session_start();
 if (!isset($_SESSION["user_name"])) {
     // redirect if not logged in
-    header("Location: html/login.html");
+    header("Location: login.html");
     exit;
 }
 
